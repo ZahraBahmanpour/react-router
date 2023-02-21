4c900659625f8a7141ed078068850d11650dea8c
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-02-21 20:37:31 +0330

    react router basics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <BrowserRouter>
+      <header>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="about">
+      <h2>About us</h2>
+      <p>this is about page</p>
+    </div>
+  );
+}
